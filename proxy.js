bdcd92446b653491a3950c4364d19221d66367eb
ezztechinc <121609267+ezztechinc@users.noreply.github.com>
2025-01-24 14:22:46 +0700

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,7 +1,13 @@
 module.exports = [
-    'http://proxy1:port',
-    'http://proxy2:port',
-    'proxy:port',
-    'https://user:pass@proxy:port',
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060'
+    'http://efge95ews4ytkdx:p8dm5ncgsor42oj@rp.scrapegw.com:6060',
 	// change all this with your proxy if you want to use proxy
 ];
